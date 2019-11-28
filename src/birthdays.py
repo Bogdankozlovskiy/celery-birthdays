a59d230910e43a40d99dba4c384ef0e343149ad8
Bogdankozlovskiy <bkozlovskiy@bk.ru>
2019-11-29 02:06:36 +0300

    my fisrt commit

---
+++ src/birthdays.py
@@ -0,0 +1,26 @@
+from celery import Celery
+from time import sleep
+#celery -A birthdays worker --pool=solo --loglevel=info      рабочие процессы
+#celery -A birthdays beat -l info                            планировщик ритмов
+
+app = Celery(
+    'birthdays', 
+    broker = 'redis://redis:6379/0'
+    )
+
+app.conf.update(
+    result_backend = 'redis://redis:6379/0'
+    )
+
+@app.task
+def birthdays_today():
+    return 'hello world matherfucker!'
+
+
+#add "birthdays_today" task to the beat schedule
+app.conf.beat_schedule = {
+    "birthday-task": {
+        "task": "birthdays.birthdays_today",
+        "schedule": 30.0,
+   }
+}
