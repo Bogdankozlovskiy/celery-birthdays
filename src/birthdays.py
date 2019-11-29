58d98c862e7910718db696521abe5d4068a6c763
Bogdankozlovskiy <bkozlovskiy@bk.ru>
2019-11-29 04:24:17 +0300

    add test row for env

--- src/birthdays.py
+++ src/birthdays.py
@@ -1,7 +1,26 @@
 from celery import Celery
 from time import sleep
+from psycopg2 import connect
+import os
 #celery -A birthdays worker --pool=solo --loglevel=info      рабочие процессы
 #celery -A birthdays beat -l info                            планировщик ритмов
+
+NAME = os.environ.get("DB_NAME")
+USER = os.environ.get("DB_USER")
+PASSWORD = os.environ.get("PGPASSWORD")
+HOST = os.environ.get("DB_HOST")
+PORT = os.environ.get("DB_PORT")
+
+print('*' * 25)
+print(NAME, USER, PASSWORD, HOST, PORT)
+print('*' * 25)
+conn = connect(
+	host = HOST,
+	port = PORT,
+	database = NAME,
+	user = USER,
+	password = PASSWORD
+	)
 
 app = Celery(
     'birthdays', 
